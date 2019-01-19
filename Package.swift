1c158ccffc9084262455e6c2325b4e25f24a4bdd
Jacob Sikorski <jacob.sikorski@gmail.com>
2019-01-19 00:31:51 -0500

    Update MapCodableKit package

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/OpenKitten/MongoKitten.git", from: "4.0.0"),
-        .package(url: "https://github.com/cuba/MapCodableKit.git", from: "1.2.0")
+        .package(url: "https://github.com/cuba/MapCodableKit.git", from: "1.2.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
